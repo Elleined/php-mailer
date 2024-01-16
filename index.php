eb9947f3d915f7e728a661ee8b5daadb48272c75
Elleined <demadegu@gmail.com>
2024-01-16 18:05:05 +0800

    added html template for sending email

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     require "./vendor/phpmailer/phpmailer/src/PHPMailer.php";
     require "./vendor/phpmailer/phpmailer/src/SMTP.php";
 
-    const GMAIL_APP_PASS = getenv("GMAIL_APP_PASS");
+    const GMAIL_APP_PASS =  "asd";
+    //getenv("GMAIL_APP_PASS");
     const YOUR_EMAIL = "demadegu@gmail.com";
-
 
     // Server settings
     $mail = new PHPMailer(true);
@@ -31,7 +31,30 @@
     // Content
     $mail-> Subject = 'Here is the subject';
     $mail-> Body = 'This is the HTML message body <b> in bold!</b> ';
-    $mail-> AltBody = 'This is the body in plain text for non-HTML mail clients';
     // $mail->send();
     echo 'Message has been sent';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Mailer</title>
+</head>
+<body>
+    <label for="email">Email: </label>
+    <input type="email" name="receiver" id="email"/><br>
+
+    <label for="subject">Subject: </label>
+    <input type="text" name="subject" id="subject"/><br>
+
+    <label for="body">Body: </label>
+    <input type="text" name="body" id="body"/> <br>
+
+    <label for="attachment">Attachment: </label>
+    <input type="file" name="attachment" id="attachment"/><br>
+
+    <input type="submit" name="Send mail" />
+</body>
+</html>
